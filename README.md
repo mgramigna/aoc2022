5b683d8c8714d414dab5932f53ff54ac738d4faf
mgramigna <mgramigna@mitre.org>
2022-12-28 10:21:02 -0500

    Day 12 Problem 2

--- README.md
+++ README.md
@@ -37,3 +37,4 @@ Solutions:
   - [Part 2](https://github.com/mgramigna/aoc2022/blob/master/src/bin/day11_2.rs)
 - Day 12
   - [Part 1](https://github.com/mgramigna/aoc2022/blob/master/src/bin/day12_1.rs)
+  - [Part 2](https://github.com/mgramigna/aoc2022/blob/master/src/bin/day12_2.rs)
---
+++ src/bin/day12_2.rs
@@ -0,0 +1,122 @@
+use std::collections::{HashSet, VecDeque};
+
+static ALPHABET: &str = "abcdefghijklmnopqrstuvwxyz";
+
+fn get_neighbors(curr: &(usize, usize), grid: &Vec<Vec<usize>>) -> Vec<(usize, usize)> {
+    let mut result = Vec::new();
+    let rows = grid.len();
+    let cols = grid[0].len();
+
+    if curr.0 > 0 {
+        result.push((curr.0 - 1, curr.1));
+    }
+
+    if curr.0 < rows - 1 {
+        result.push((curr.0 + 1, curr.1));
+    }
+
+    if curr.1 > 0 {
+        result.push((curr.0, curr.1 - 1));
+    }
+
+    if curr.1 < cols - 1 {
+        result.push((curr.0, curr.1 + 1));
+    }
+
+    result
+}
+
+#[derive(Debug)]
+struct Cell {
+    row: usize,
+    col: usize,
+    dist: usize,
+}
+
+fn get_shortest_path(
+    grid: &Vec<Vec<usize>>,
+    start: &(usize, usize),
+    end: &(usize, usize),
+) -> usize {
+    let mut visited: HashSet<(usize, usize)> = HashSet::new();
+    let mut queue: VecDeque<Cell> = VecDeque::new();
+
+    queue.push_back(Cell {
+        row: start.0,
+        col: start.1,
+        dist: 0,
+    });
+
+    visited.insert(*start);
+
+    while let Some(Cell { row, col, dist }) = queue.pop_front() {
+        if (row, col) == *end {
+            return dist as usize;
+        }
+
+        let neighbors: Vec<(usize, usize)> = get_neighbors(&(row, col), &grid);
+
+        let curr_height: usize = grid[row][col].try_into().unwrap();
+        let valid_neighbors: Vec<&(usize, usize)> = neighbors
+            .iter()
+            .filter(|n| {
+                let neighbor_height = grid[n.0][n.1];
+
+                return neighbor_height <= curr_height || neighbor_height == curr_height + 1;
+            })
+            .collect();
+
+        for n in valid_neighbors {
+            if !visited.contains(n) {
+                queue.push_back(Cell {
+                    row: n.0,
+                    col: n.1,
+                    dist: dist + 1,
+                });
+                visited.insert(*n);
+            }
+        }
+    }
+    0
+}
+
+fn main() {
+    let input = include_str!("../inputs/day12.txt");
+
+    let mut end: (usize, usize) = (0, 0);
+    let mut grid: Vec<Vec<usize>> = vec![];
+
+    for (row_idx, row_str) in input.lines().enumerate() {
+        let mut col: Vec<usize> = vec![];
+        for (col_idx, col_ch) in row_str.chars().enumerate() {
+            if col_ch == 'S' {
+                col.push(0);
+            } else if col_ch == 'E' {
+                end = (row_idx, col_idx);
+                col.push(25);
+            } else {
+                col.push(ALPHABET.find(col_ch).unwrap());
+            }
+        }
+        grid.push(col);
+    }
+
+    let mut start_candidates: Vec<(usize, usize)> = vec![];
+
+    for row in 0..grid.len() {
+        for col in 0..grid[0].len() {
+            if grid[row][col] == 0 {
+                start_candidates.push((row, col));
+            }
+        }
+    }
+
+    let min_shortest_path: usize = start_candidates
+        .iter()
+        .map(|c| get_shortest_path(&grid, c, &end))
+        .filter(|i| *i != 0)
+        .min()
+        .unwrap();
+
+    println!("{}", min_shortest_path);
+}
